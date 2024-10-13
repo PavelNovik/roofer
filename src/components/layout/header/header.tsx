dc9f31448bf943b2defdbfdc7a7fb89999ebf29e
Pavel Novik <brn2befree@gmail.com>
2024-10-13 23:20:22 +0300

    add menu list

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,10 +1,20 @@
 import { Logo } from '@/components/ui/logo/logo'
+import { Typography } from '@/components/ui/typography/typography'
+import s from './header.module.scss'
 
 export const Header = () => {
   return (
     <header style={{ marginTop: '20px' }}>
       <Logo />
+      <div className={s.menuList}>
+        {menuList.map(i => (
+          <Typography>{i}</Typography>
+        ))}
+      </div>
+      <br />
       This is HEADER. <b>This project has been created for my Polish friend Błarzej!</b>
     </header>
   )
 }
+
+const menuList = ['home', 'about', 'news', 'contact', 'services', 'projects']
