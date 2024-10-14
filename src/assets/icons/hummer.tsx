fcfeab26acebd2279a3ed3b3707cba7962846f6d
Pavel Novik <brn2befree@gmail.com>
2024-10-14 21:16:11 +0300

    add hummer icon

--- src/assets/icons/hummer.tsx
+++ src/assets/icons/hummer.tsx
@@ -2,9 +2,9 @@ import { Ref, SVGProps, forwardRef } from 'react'
 const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
     fill={'none'}
-    width="82.500000"
-    height="100.833374"
-    viewBox="0 0 82.5 100.833"
+    width="66.666656"
+    height="62.541626"
+    viewBox="0 0 66.6667 62.5416"
     xmlns={'http://www.w3.org/2000/svg'}
     {...props}
     ref={ref}
@@ -13,7 +13,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
     <defs />
     <path
       id="Vector"
-      d="M82.5 45.83C82.5 71.27 64.9 95.05 41.25 100.83C17.6 95.05 0 71.27 0 45.83L0 18.33L41.25 0L82.5 18.33L82.5 45.83ZM41.25 91.66C58.43 87.08 73.33 66.64 73.33 46.84L73.33 24.29L41.25 9.99L9.16 24.29L9.16 46.84C9.16 66.64 24.06 87.08 41.25 91.66ZM36.66 59.58L45.83 59.58L45.83 73.33L59.58 73.33L59.58 50.41L68.75 50.41L41.25 27.5L13.75 50.41L22.91 50.41L22.91 73.33L36.66 73.33L36.66 59.58Z"
+      d="M0 55.44L38.09 17.34L35.73 15L40.46 10.24L33.33 2.97C37.33 -1 43.63 -1 47.56 2.97L59.56 15L54.83 19.7L64.3 19.7L66.66 22.07L54.83 34.04L52.46 31.67L52.46 22.07L47.56 26.8L45.2 24.44L7.1 62.54L0 55.44Z"
       fill="#D06051"
       fill-opacity="1.000000"
       fill-rule="nonzero"
@@ -22,4 +22,4 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
 )
 const ForwardRef = forwardRef(SvgComponent)
 
-export const Guard = ForwardRef
+export const Hummer = ForwardRef
