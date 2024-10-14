fb0aaf67ba6d29c25f1273c3cc212e533532621e
Pavel Novik <brn2befree@gmail.com>
2024-10-14 23:10:49 +0300

    change some starting logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import { Header } from '@/components/layout/header/header'
 import { Footer } from '@/components/layout/footer/footer'
-import { ArrowDown } from '@/assets/icons/arrow_down'
-import { ArrowUp } from '@/assets/icons/arrow_up'
-import { Phone1 } from '@/assets/icons/phone1'
-import { Star } from '@/assets/icons/star'
+import { Button } from '@/components/ui/button/button'
 
 export function App() {
   return (
     <>
       <Header />
+      <Button>Push me</Button>
       <div>Hello mother fucker. Here will be the project about roofers!</div>
-      <ArrowDown />
-      <ArrowUp />
-      <Phone1 />
-      <Star />
-      <Star />
-      <Star />
       <Footer />
     </>
   )
