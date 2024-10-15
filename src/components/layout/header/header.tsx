b2fd201c4155c9f6513ba4bfaf0c14cedeae2056
Pavel Novik <brn2befree@gmail.com>
2024-10-15 22:14:21 +0300

    some check

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,17 +1,21 @@
 import { Logo } from '@/components/ui/logo/logo'
 import { Typography } from '@/components/ui/typography/typography'
 import s from './header.module.scss'
+import { PartnerLogo } from '@/components/ui/logo/partner-logo'
 
 export const Header = () => {
   return (
     <header style={{ marginTop: '20px' }}>
       <Logo />
-      <div className={s.menuList}>
+      <ul className={s.menuList}>
         {menuList.map(i => (
-          <Typography key={i}>{i}</Typography>
+          <li key={i}>
+            <Typography>{i}</Typography>
+          </li>
         ))}
-      </div>
+      </ul>
       <br />
+      <PartnerLogo />
       This is HEADER. <b>This project has been created for my Polish friend Błarzej!</b>
     </header>
   )
