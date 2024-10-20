d818fd899d53f639e038fd8ec7ea26a0711f829c
Pavel Novik <brn2befree@gmail.com>
2024-10-20 14:46:29 +0300

    change style import logic

--- src/components/ui/logo/logo.tsx
+++ src/components/ui/logo/logo.tsx
@@ -2,15 +2,18 @@ import { Logo1 } from '@/assets/icons/logo1'
 import { Logo2 } from '@/assets/icons/logo2'
 import s from './logo.module.scss'
 import { Typography } from '@/components/ui/typography/typography'
+import clsx from 'clsx'
+type LogoT = {
+  className?: string
+}
 
-export const Logo = () => {
+export const Logo = (props: LogoT) => {
+  const { className } = props
   return (
-    <div className={s.logoBox}>
-      <div className="wwww">
-        <div className={s.logo}>
-          <Logo2 />
-          <Logo1 />
-        </div>
+    <div className={clsx(className, s.logoBox)}>
+      <div className={s.logo}>
+        <Logo2 />
+        <Logo1 />
       </div>
 
       <div>
