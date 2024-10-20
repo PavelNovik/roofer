819a4ddd50e46e19f19a40cf62a4d1ce8975b632
Pavel Novik <brn2befree@gmail.com>
2024-10-20 21:32:55 +0300

    add about section and styles for it

--- src/components/layout/about/about.tsx
+++ src/components/layout/about/about.tsx
@@ -1,5 +1,41 @@
 import s from './about.module.scss'
 import { Section } from '@/components/ui/section/section'
+import { Typography } from '@/components/ui/typography/typography'
+import clsx from 'clsx'
+import { Point } from '@/assets/icons/point'
+import { Form } from '@/components/ui/form/form'
+
 export const About = () => {
-  return <Section className={s.about} id={'about'}></Section>
+  return (
+    <Section className={s.about} id={'about'}>
+      <div className={s.content}>
+        <Typography className={clsx(s.sectionTitle)}>QUALITY ROOFING SOLUTIONS</Typography>
+        <Typography as={'h1'} className={clsx(s.mainTitle)}>
+          Expert roofers at your command
+        </Typography>
+        <Typography className={clsx(s.aboutParagraph)}>
+          Sagittis pulvinar ut dis venenatis nunc nunc vitae aliquam vestibulum. Elit adipiscing
+          massa diam in dignissim. Risus tellus libero elementum aliquam etiam. Lectus adipiscing
+          est auctor mi quisque nunc non viverra est.
+        </Typography>
+        <ul className={clsx(s.aboutList)}>
+          <li>
+            <Point />
+            Fully legal licensed and insured
+          </li>
+          <li>
+            {' '}
+            <Point />
+            100% trustworthy company
+          </li>
+          <li>
+            {' '}
+            <Point />
+            Most Genuine and transparent
+          </li>
+        </ul>
+      </div>
+      <Form />
+    </Section>
+  )
 }
