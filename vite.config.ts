909d6f33fc8a826a0b0382551c3558e84fa78065
Pavel Novik <brn2befree@gmail.com>
2024-10-20 14:47:10 +0300

    change sass config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler', // or "modern", "legacy"
+        importers: [
+          // ...
+        ],
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
