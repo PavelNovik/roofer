415d324f41776ceed7146e603b447d9afd5462ff
Pavel Novik <brn2befree@gmail.com>
2024-10-20 14:45:53 +0300

    add about section and style

---
+++ src/components/layout/about/about.tsx
@@ -0,0 +1,5 @@
+import s from './about.module.scss'
+import { Section } from '@/components/ui/section/section'
+export const About = () => {
+  return <Section className={s.about} id={'about'}></Section>
+}
