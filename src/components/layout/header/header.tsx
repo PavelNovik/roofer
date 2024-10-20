ace3d3e9c47c897e6047bb3e70660ebdbc898050
Pavel Novik <brn2befree@gmail.com>
2024-10-20 14:47:25 +0300

    fix header logic and styles

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,22 +1,16 @@
 import { Logo } from '@/components/ui/logo/logo'
-import { Typography } from '@/components/ui/typography/typography'
 import s from './header.module.scss'
-import { PartnerLogo } from '@/components/ui/logo/partner-logo'
+import { Menu } from '@/components/ui/menu/menu'
+import { Phone } from '@/components/ui/phone/phone'
+import { Button } from '@/components/ui/button/button'
 
 export const Header = () => {
   return (
-    <header style={{ marginTop: '20px' }}>
-      <Logo />
-      <ul className={s.menuList}>
-        {menuList.map(i => (
-          <li key={i}>
-            <Typography>{i}</Typography>
-          </li>
-        ))}
-      </ul>
-      <br />
-      <PartnerLogo />
-      This is HEADER. <b>This project has been created for my Polish friend Błarzej!</b>
+    <header className={s.header}>
+      <Logo className={s.logo} />
+      <Menu className={s.menu} menuList={menuList} />
+      <Phone className={s.phone} />
+      <Button>Get in touch</Button>
     </header>
   )
 }
