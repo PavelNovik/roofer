ea9b3701834fe3e85a2c803584cd60ae1d83145d
Pavel Novik <brn2befree@gmail.com>
2024-10-20 14:46:03 +0300

    add about container and style

---
+++ src/components/ui/container/container.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+import s from './container.module.scss'
+import clsx from 'clsx'
+
+export type ContainerT = {
+  children?: ReactNode
+  className?: string
+}
+export const Container = (props: ContainerT) => {
+  const { children, className } = props
+  return <div className={clsx(className, s.container)}>{children}</div>
+}
