07da0e2fa979b22a05adad4e1bc739a4520087f6
Pavel Novik <brn2befree@gmail.com>
2024-10-21 22:37:00 +0300

    change some wrappers logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@/components/layout/header/header'
 import { Container } from '@/components/ui/container/container'
 import { About } from '@/components/layout/about/about'
+import { Partners } from '@/components/layout/partners/partners'
 
 export function App() {
   return (
     <Container>
       <Header />
       <About />
+      <Partners />
     </Container>
   )
 }
