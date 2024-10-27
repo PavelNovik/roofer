b522d03dc165ffa4d8d79930eeb48c56f4ed4c9f
Pavel Novik <brn2befree@gmail.com>
2024-10-27 18:30:10 +0300

    add new section customers in the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from '@/components/layout/header/header'
 import { Container } from '@/components/ui/container/container'
 import { About } from '@/components/layout/about/about'
 import { Partners } from '@/components/layout/partners/partners'
+import { Customers } from '@/components/layout/customers/customers'
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <Header />
       <About />
       <Partners />
+      <Customers />
     </Container>
   )
 }
