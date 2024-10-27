f2e1d5b379f4cdf8ad3f888872e91b8eadfeb8b8
Pavel Novik <brn2befree@gmail.com>
2024-10-27 21:39:19 +0300

    change header styles

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -3,14 +3,19 @@ import s from './header.module.scss'
 import { Menu } from '@/components/ui/menu/menu'
 import { Phone } from '@/components/ui/phone/phone'
 import { Button } from '@/components/ui/button/button'
+import { Wrapper } from '@/components/ui/wrapper/wrapper'
 
 export const Header = () => {
   return (
     <header className={s.header}>
-      <Logo className={s.logo} />
-      <Menu className={s.menu} menuList={menuList} />
-      <Phone className={s.phone} />
-      <Button>Get in touch</Button>
+      <Wrapper className={s.headerContext}>
+        <Logo className={s.logo} />
+        <Wrapper className={s.headerWrapper}>
+          <Menu className={s.menu} menuList={menuList} />
+          <Phone className={s.phone} />
+          <Button>Get in touch</Button>
+        </Wrapper>
+      </Wrapper>
     </header>
   )
 }
