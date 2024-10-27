964a9e5a3fcf10d0606f7088e95f0ecc7b7cac19
Pavel Novik <brn2befree@gmail.com>
2024-10-27 21:40:49 +0300

    add service card component

---
+++ src/components/ui/serviceCard/serviceCard.tsx
@@ -0,0 +1,7 @@
+import { Card } from '@/components/ui/card/card'
+import s from './serviceCard.module.scss'
+import clsx from 'clsx'
+
+export const ServiceCard = () => {
+  return <Card className={clsx(s.serviceCard)}>dhfhsjh</Card>
+}
