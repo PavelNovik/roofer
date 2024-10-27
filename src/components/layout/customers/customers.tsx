071d0a9eb8485f913b7dc9dc59b67b6baaee4f1d
Pavel Novik <brn2befree@gmail.com>
2024-10-27 21:38:49 +0300

    change style params in customer section

--- src/components/layout/customers/customers.tsx
+++ src/components/layout/customers/customers.tsx
@@ -1,10 +1,31 @@
 import s from './customers.module.scss'
 import { Typography } from '@/components/ui/typography/typography'
+import { Wrapper } from '@/components/ui/wrapper/wrapper'
+import { ReviewCard } from '@/components/ui/reviewCard/reviewCard'
 export const Customers = () => {
   return (
     <section className={s.customers}>
-      <Typography className={s.title}>Homeowners reviews</Typography>
-      <Typography className={s.headerTitle}>Our happy customers</Typography>
+      <Wrapper className={s.customersWrapper}>
+        <Typography className={s.title}>Homeowners reviews</Typography>
+        <Typography className={s.headerTitle}>Our happy customers</Typography>
+        <Wrapper className={s.cardsWrapper}>
+          <ReviewCard
+            content={
+              '"A pellen tesque pretium feugiat vel mobi sagittis lorem habi tasse cursus ipsum quis nec eget facilisis. Quis nibh ut bindum nisl quis placerat proin tortor mattis."'
+            }
+          />
+          <ReviewCard
+            content={
+              '"Feugiat hac porttitor sem dictum velit, elementum odio. Sed ipsum quis nec eget facilisis. Quis nibh ut bibendum nisl quis placerat proin tortor mattis in neque."'
+            }
+          />
+          <ReviewCard
+            content={
+              '"Quis nibh ut bibendum nisl quis placerat proin tortor mattis in neque. Feugiat hac porttitor sem dictum velit, elementum odio sed eget facilisis."'
+            }
+          />
+        </Wrapper>
+      </Wrapper>
     </section>
   )
 }
