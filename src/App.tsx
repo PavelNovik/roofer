9e00a4aec6d5d1139af7282cc9f8e1cbab6f3875
Pavel Novik <brn2befree@gmail.com>
2024-10-27 21:38:00 +0300

    add new sections

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Container } from '@/components/ui/container/container'
 import { About } from '@/components/layout/about/about'
 import { Partners } from '@/components/layout/partners/partners'
 import { Customers } from '@/components/layout/customers/customers'
+import { Service } from '@/components/layout/service/service'
 
 export function App() {
   return (
@@ -11,6 +12,7 @@ export function App() {
       <About />
       <Partners />
       <Customers />
+      <Service />
     </Container>
   )
 }
